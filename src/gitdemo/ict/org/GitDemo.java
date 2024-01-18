c3eb118c6e8ae5504e829f015a2ca3187c0b79b6
my <my@DESKTOP-MJJQUKN>
2024-01-18 13:42:44 +0530

    First commit..

---
+++ src/gitdemo/ict/org/GitDemo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package gitdemo.ict.org;
+
+/**
+ * 
+ */
+public class GitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		System.out.println("Stored value is "+num1);
+	boolean flag = true;
+	System.out.println("Boolean value stored is " +flag);
+
+	}
+
+}
